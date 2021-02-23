4526d80d6f3c1f9c6bdc4eb53ba3b2134632c4fe
Макар Зарчуков <makzarchukov@gmail.com>
2021-02-23 12:32:37 +0500

    tic-tac-toe task

---
+++ index.js
@@ -0,0 +1,89 @@
+const CROSS = 'X';
+const ZERO = 'O';
+const EMPTY = ' ';
+
+const container = document.getElementById('fieldWrapper');
+
+startGame();
+addResetListener();
+
+function startGame () {
+    renderGrid(3);
+}
+
+function renderGrid (dimension) {
+    container.innerHTML = '';
+
+    for (let i = 0; i < dimension; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < dimension; j++) {
+            const cell = document.createElement('td');
+            cell.textContent = EMPTY;
+            cell.addEventListener('click', () => cellClickHandler(i, j));
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+}
+
+function cellClickHandler (row, col) {
+    // Пиши код тут
+    console.log(`Clicked on cell: ${row}, ${col}`);
+
+
+    /* Пользоваться методом для размещения символа в клетке так:
+        renderSymbolInCell(ZERO, row, col);
+     */
+}
+
+function renderSymbolInCell (symbol, row, col, color = '#333') {
+    const targetCell = findCell(row, col);
+
+    targetCell.textContent = symbol;
+    targetCell.style.color = color;
+}
+
+function findCell (row, col) {
+    const targetRow = container.querySelectorAll('tr')[row];
+    return targetRow.querySelectorAll('td')[col];
+}
+
+function addResetListener () {
+    const resetButton = document.getElementById('reset');
+    resetButton.addEventListener('click', resetClickHandler);
+}
+
+function resetClickHandler () {
+    console.log('reset!');
+}
+
+
+/* Test Function */
+/* Победа первого игрока */
+function testWin () {
+    clickOnCell(0, 2);
+    clickOnCell(0, 0);
+    clickOnCell(2, 0);
+    clickOnCell(1, 1);
+    clickOnCell(2, 2);
+    clickOnCell(1, 2);
+    clickOnCell(2, 1);
+}
+
+/* Ничья */
+function testDraw () {
+    clickOnCell(2, 0);
+    clickOnCell(1, 0);
+    clickOnCell(1, 1);
+    clickOnCell(0, 0);
+    clickOnCell(1, 2);
+    clickOnCell(1, 2);
+    clickOnCell(0, 2);
+    clickOnCell(0, 1);
+    clickOnCell(2, 1);
+    clickOnCell(2, 2);
+}
+
+function clickOnCell (row, col) {
+    findCell(row, col).click();
+}
