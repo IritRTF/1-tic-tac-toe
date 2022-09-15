9370df0c23e7acd04fb511cfbfd81b49ee75f684
IuxMorti <zdracte21@bk.ru>
2022-09-15 17:52:08 +0500

    на чем закончили на паре

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
-
+let index = 1;  //счетчик, какой ход по счету. Неч - CROSS, чет ZERO
 const container = document.getElementById('fieldWrapper');
+const field = new Array();
 
 startGame();
 addResetListener();
+
+function createEmptyField(dimension) {
+    field.length = 0;
+
+    for (let i = 0; i < dimension; i++)
+        field[i] = []
+
+    for (let i = 0; i < dimension; i++)
+    for (let j = 0; j < dimension; j++) {
+        field[i][j] = EMPTY
+    }
+}
+
+function  
 
 function startGame () {
     renderGrid(3);
 }
 
-function renderGrid (dimension) {
+function ()
+
+function renderrid (dimension) {
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
@@ -29,8 +46,9 @@ function renderGrid (dimension) {
 function cellClickHandler (row, col) {
     // Пиши код тут
     console.log(`Clicked on cell: ${row}, ${col}`);
-
-
+    if (field[row][col] == EMPTY) {
+        
+    }
     /* Пользоваться методом для размещения символа в клетке так:
         renderSymbolInCell(ZERO, row, col);
      */
@@ -61,27 +79,75 @@ function resetClickHandler () {
 /* Test Function */
 /* Победа первого игрока */
 function testWin () {
-    clickOnCell(0, 2);
-    clickOnCell(0, 0);
-    clickOnCell(2, 0);
-    clickOnCell(1, 1);
-    clickOnCell(2, 2);
-    clickOnCell(1, 2);
-    clickOnCell(2, 1);
+    clickOnCell(0, 2);//x
+    //    x
+    //
+    //    
+    clickOnCell(0, 0);//o
+    //o   x
+    //
+    // 
+    clickOnCell(2, 0);//x
+    //o   x
+    //  
+    //x 
+    clickOnCell(1, 1);//o
+    //o   x
+    //  o
+    //x   
+    clickOnCell(2, 2);//x
+    //o   x
+    //  o
+    //x   x 
+    clickOnCell(1, 2);//o
+    //o   x
+    //  o o
+    //x   x
+    clickOnCell(2, 1);//x
+    //o   x
+    //  o o
+    //x x x
 }
 
 /* Ничья */
 function testDraw () {
-    clickOnCell(2, 0);
-    clickOnCell(1, 0);
-    clickOnCell(1, 1);
-    clickOnCell(0, 0);
-    clickOnCell(1, 2);
-    clickOnCell(1, 2);
-    clickOnCell(0, 2);
-    clickOnCell(0, 1);
-    clickOnCell(2, 1);
-    clickOnCell(2, 2);
+    clickOnCell(2, 0); //x
+    //| | | |
+    //| | | |
+    //|x| | | 
+    clickOnCell(1, 0); //o
+    //| | | |
+    //|o| | |
+    //|x| | |
+    clickOnCell(1, 1); //x
+    //| | | |
+    //|o|x| |
+    //|x| | |
+    clickOnCell(0, 0); //o
+    //|o| | |
+    //|o|x| |
+    //|x| | |
+    clickOnCell(1, 2); //x
+    //|o| | |
+    //|o|x|x|
+    //|x| | |
+    clickOnCell(1, 2); //o
+    //|o| | |
+    //|o|x|x| ????
+    //|x| | |
+    clickOnCell(0, 2); //x
+    //|o| |x|
+    //|o|x|x| ????
+    //|x| | |
+    clickOnCell(0, 1); //o
+    //|o|o|x|
+    //|o|x|x| ????
+    //|x| | |
+    clickOnCell(2, 1); //x
+    //|o|o|x|
+    //|o|x|x o| ????
+    //|x| | |
+    clickOnCell(2, 2); //o
 }
 
 function clickOnCell (row, col) {
