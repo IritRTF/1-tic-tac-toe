ccc0e602115b82b8ec823c5caec89644b8e8c0ef
Kostya <mikril10426@gmail.com>
2022-09-19 23:13:49 +0500

    T

--- index.js
+++ index.js
@@ -4,11 +4,52 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
-startGame();
+let currentPlayer = CROSS;
+let countMove = 0;
+let endGame = false;
+let winCombination = new Array;
+
+class Point{
+    constructor(row, col){
+        this.row = row;
+        this.col = col;
+    }
+}
+
+let sizeGrid = Number (prompt("Введите размер поля", 3));
+let freeCellArray = fillArrayWithPoints(sizeGrid);
+let cellArray = new Array(sizeGrid);
+
+createCellDoubArr();
+startGame(sizeGrid);
 addResetListener();
 
-function startGame () {
-    renderGrid(3);
+function fillArrayWithPoints(size){
+    let array = new Array;
+    for (let i = 0; i < size; i++)
+    for (let j = 0; j < size; j++)
+    array.push(new Point(i, j));
+    return array;
+}
+
+function deletePoint(point){
+    for (let i = 0; i < freeCellArray.length; i++)
+        if (freeCellArray[i].row === point.row && freeCellArray[i].col === point.col){
+            freeCellArray.splice(i, 1);
+            break;
+        }
+}
+
+function createCellDoubArr(){
+    for (let i = 0; i < cellArray.length; i++){
+        cellArray[i] = new Array(sizeGrid);
+            for(let j = 0; j < cellArray[i].length; j++)
+            cellArray[i][j] = EMPTY;
+    }
+}
+
+function startGame (size) {
+    renderGrid(size); 
 }
 
 function renderGrid (dimension) {
@@ -19,7 +60,7 @@ function renderGrid (dimension) {
         for (let j = 0; j < dimension; j++) {
             const cell = document.createElement('td');
             cell.textContent = EMPTY;
-            cell.addEventListener('click', () => cellClickHandler(i, j));
+            cell.addEventListener('click', () => cellClickHandler(i, j) );
             row.appendChild(cell);
         }
         container.appendChild(row);
@@ -27,13 +68,144 @@ function renderGrid (dimension) {
 }
 
 function cellClickHandler (row, col) {
-    // Пиши код тут
+    
+    
+    
+    if(!endGame && findCell(row, col).textContent == EMPTY){
+        currentPlayer = CROSS;
+        renderSymbolInCell(currentPlayer, row, col);
+        cellArray[row][col] = currentPlayer;
+        deletePoint(new Point(row, col));
+        if(countMove >= 4) checkWin();
+        countMove++; 
+        if (!endGame){
+            currentPlayer = ZERO;
+            let point = freeCellArray[Math.floor(Math.random() * freeCellArray.length)];
+            renderSymbolInCell(currentPlayer, point.row, point.col);
+            deletePoint(point);
+            cellArray[point.row][point.col] = currentPlayer;
+            if(countMove >= 4) checkWin();
+            countMove++; 
+        }
+    }
+
     console.log(`Clicked on cell: ${row}, ${col}`);
+    console.log(cellArray[row][col]);
+    console.log(freeCellArray);
+    
+    if(countMove == sizeGrid ** 2) {
+        endGame = true;
+        alert("Победила дружба");
+    }
+}
 
+function checkWin(){
+    checkWinHorizontal();
+    checkWinVertical();
+    checkWinDiagonal();
+}
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+function checkWinHorizontal(){
+    for(let x = 0; x < cellArray.length; x++){
+        let flag = true;
+        
+        if(cellArray[x][0] == EMPTY){ 
+            flag = false;
+            continue;
+        }
+
+        if(cellArray[x][0] != currentPlayer){
+            flag = false;
+            continue;
+        }
+
+        for(let y = 0; y < cellArray[x].length; y++){
+            if(cellArray[x][0] != cellArray[x][y]){
+                flag = false;
+                winCombination = [];
+                break;
+            }
+            else winCombination.push([x, y]);
+        }
+        if(flag) {
+            endGame = true;
+            paintOverWinComb();
+            alert(`Победил ${currentPlayer}`);
+            break;
+        }
+    }
+}
+
+function checkWinVertical(){
+    for(let y = 0; y < cellArray[0].length; y++){
+        let flag = true;
+        
+        if(cellArray[0][y] == EMPTY){ 
+            flag = false;
+            continue;
+        }
+
+        if(cellArray[0][y] != currentPlayer){
+            flag = false;
+            continue;
+        }
+
+        for(let x = 0; x < cellArray[y].length; x++){
+            if(cellArray[0][y] != cellArray[x][y]){
+                flag = false;
+                winCombination = [];
+                break;
+            }
+            else winCombination.push([x, y]);
+        }
+        if(flag) {
+            endGame = true;
+            paintOverWinComb();
+            alert(`Победил ${currentPlayer}`);
+            break;
+        }
+    }
+}
+
+function checkWinDiagonal(){
+    let flag = true;
+    let upRightPoint = cellArray.length - 1;
+    
+    if(cellArray[0][0] == currentPlayer){
+        for(let i = 0; i < cellArray.length; i++){
+            if(cellArray[i][i] != currentPlayer){
+                flag = false;
+                winCombination = [];
+                break;
+            }
+            else winCombination.push([i, i]);
+        }
+    }
+    else if(cellArray[0][upRightPoint] == currentPlayer){
+        for(let i = 0; i < cellArray.length; i++){
+            if(cellArray[i][upRightPoint - i] != currentPlayer){
+                flag = false;
+                winCombination = [];
+                break;
+            }
+            else winCombination.push([i, upRightPoint - i]);
+        }
+    }
+    else 
+        flag = false;
+
+    if(flag) {
+        endGame = true;
+        paintOverWinComb();
+        alert(`Победил ${currentPlayer}`);
+    }
+}
+
+function paintOverWinComb(){
+    for(let e of winCombination){
+        let cell = findCell(e[0], e[1]);
+        cell.style.color = 'red';
+    }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -54,6 +226,12 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
+    endGame = false;
+    countMove = 0;
+    currentPlayer = CROSS;
+    createCellDoubArr();
+    freeCellArray = fillArrayWithPoints(sizeGrid);
+    renderGrid(sizeGrid);
     console.log('reset!');
 }
 
