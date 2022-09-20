4b8765d61c159b9fc3fff1ad17120bc914f3c3a4
VladislavPHATEEV <involvedbrax@gmail.com>
2022-09-20 23:03:52 +0500

    Task done

--- index.js
+++ index.js
@@ -1,14 +1,145 @@
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
+const RED_COLOR = '#F00'
+
+const UPLEFT =  new Vector(-1, -1);
+const UP = new Vector(-1, 0);
+const UPRIGHT = new Vector(-1, 1);
+const RIGHT = new Vector(0, 1);
+const DOWNRIGHT = new Vector(1, 1);
+const DOWN = new Vector(1, 0);
+const DOWNLEFT = new Vector(1, -1);
+const LEFT = new Vector(0, -1);
+
+const DIRECTIONS = [UPLEFT, UP, UPRIGHT, RIGHT, DOWNRIGHT, DOWN, DOWNLEFT, LEFT]; 
 
 const container = document.getElementById('fieldWrapper');
+
+let onAI = false;
+let isAdvanceAI = false;
+let isFieldExtensionEnabled = false;
+
+let allCell = [];
+let field = [];
+
+let winLength = 3;
+let fieldSize = 3;
+let moveCounter = 0;
+let isGameOver = false;
+
 
 startGame();
 addResetListener();
 
+function getEmptyCells () {
+    let result = [];
+    for (let i = 0; i < fieldSize; i++) {
+        for (let j = 0; j < fieldSize; j++) {
+            if (field[i][j] === EMPTY) result.push(new Vector(i, j));
+        }
+    }
+    return result;
+}
+
+function updateListCell(){
+    allCell = shuffle(getEmptyCells());
+}
+
 function startGame () {
-    renderGrid(3);
+
+    if (!isFieldExtensionEnabled){
+        isFieldExtensionEnabled = confirm("Включить расширение поля?");
+    }
+    if(!onAI){
+        if(confirm("Включить AI?")){
+            isAdvanceAI = confirm("Включить умный AI?");
+            onAI = true;
+        }
+    }
+    isGameOver = false;
+    field = []
+    fieldSize = getStartFieldSize();
+    //winLength = Math.Min(5, fieldSize); // если вдруг не хотим делать серии из 3 на большом поле 
+    moveCounter = 0;
+    createField(fieldSize)
+    renderGrid(fieldSize);
+    updateListCell();
+}
+
+function getStartFieldSize () {
+    let result = "";
+    result = prompt("Введите стартовое значение размера поля.", 3);
+    while (isNaN(result)) {
+        result = prompt("Неверный ввод данных. Повторите попытку — введите число.", 3);
+    }
+    return result;
+}
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+
+function GetCurrentSymbolPlayer (moveCounter) {
+    return moveCounter % 2 == 0 ? CROSS: ZERO;
+}
+
+function isWithinField (point) {
+    return point.x >= 0 && point.x < fieldSize && point.y >= 0 && point.y < fieldSize;
+}
+
+function getNeighbourCells (x, y) {
+    let result = [];
+    for (let direction of DIRECTIONS) {
+        let point =  new Vector(x + direction.x, y + direction.y);
+        if (isWithinField(point)){
+            result.push(point);
+        }
+    }
+    return result;
+}
+
+function createField (fieldSize) {
+    for (let i = 0; i < fieldSize; ++i) {
+        let row = []
+        for (let j = 0; j < fieldSize; ++j) {
+            row.push(EMPTY);
+        }
+        field.push(row);
+    }
+}
+
+function extendField () {
+    let row = []
+    for (let i = 0; i < fieldSize; ++i) {
+        row.push(EMPTY);
+    }
+    field.push(row);
+    for (let i = 0; i <= fieldSize; ++i) {
+        field[i][fieldSize] = EMPTY;
+    }
+    ++fieldSize;
+}
+
+function renderExtendedField () {
+    extendField();
+    renderGrid(fieldSize);
+    for (let i = 0; i < fieldSize; ++i) {
+        for (let j = 0; j < fieldSize; ++j) {
+            renderSymbolInCell(field[i][j], i, j);
+        }
+    }
 }
 
 function renderGrid (dimension) {
@@ -27,13 +158,121 @@ function renderGrid (dimension) {
 }
 
 function cellClickHandler (row, col) {
-    // Пиши код тут
     console.log(`Clicked on cell: ${row}, ${col}`);
 
+    if (isGameOver || field[row][col] !== EMPTY) return;
+    
+    makeMove(new Vector(row, col), GetCurrentSymbolPlayer(moveCounter));
+    
+}
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+function makeMove(point, symbol){
+    if(isGameOver) return;
+
+    field[point.x][point.y] = symbol;
+    renderSymbolInCell(symbol, point.x, point.y);
+    moveCounter++;
+    let line = findWinLineInPosition(point.x, point.y, symbol);
+    if (line){
+        announceWinner(line, onAI && symbol === ZERO ? "AI": `Player${symbol === CROSS? 1: 2}`);
+        return;
+    }
+    updateGameStatus();
+
+    if(onAI && symbol == CROSS)
+        makeMove(getMoveAI(), GetCurrentSymbolPlayer(moveCounter));
+}
+
+function getMoveAI(){
+    return isAdvanceAI? getAdvanceMoveAI(): getRandomMove();
+
+}
+
+function getRandomMove(){
+    let result = allCell.pop();
+    while(field[result.x][result.y] !== EMPTY && allCell.length !== 0){
+        result = allCell.pop();
+    }
+    return result;
+}
+
+function getAdvanceMoveAI(){
+    return findWinnerCell(ZERO) ?? getRandomMove();
+}   
+
+function announceWinner(winner, nameWinne){
+    let symbol = field[winner[0].x][winner[0].y];
+    renderVictorySymbols(symbol, winner, RED_COLOR);
+    alert(`Победил ${nameWinne}`);
+    isGameOver = true;
+}
+
+function findWinnerCell (symbol) {
+    let EmtyCells = getEmptyCells();
+    main: for (let i = 0; i < EmtyCells.length; ++i) {
+            if (findWinLineInPosition(EmtyCells[i].x, EmtyCells[i].y, symbol))
+                return EmtyCells[i];
+    }
+    return null;
+}
+
+function findWinLineInPosition(x, y, symbol){
+    let neighbourCells = getNeighbourCells(x, y);
+    for (let neighbourCell of neighbourCells) {
+        let direction = new Vector(neighbourCell.x - x, neighbourCell.y - y);
+        let line = createLine(new Vector(x, y), direction, symbol);
+        if(line.length >= winLength)
+            return line; 
+    }
+    return null;
+}
+
+function createLine(point, direction, symbol){
+    let result = [point];
+    let isPossibleToGoInDir = true;
+    let isPossibleToGoAgainstDir = true;
+
+    for (let step = 1; step < winLength; step++){
+        if (!isPossibleToGoInDir && !isPossibleToGoAgainstDir) break;
+
+        if(isPossibleToGoInDir){
+            isPossibleToGoInDir = addPointInLine(new Vector(point.x + step * direction.x,
+                                              point.y + step * direction.y),
+                                   symbol, result);
+        }
+        if (isPossibleToGoAgainstDir){
+            isPossibleToGoAgainstDir = addPointInLine(new Vector(point.x - step * direction.x,
+                                              point.y - step * direction.y),
+                                   symbol, result)
+        }
+    }
+    return result;
+}
+
+function addPointInLine(point, symbol, line){
+    if (isWithinField(point) && field[point.x][point.y] === symbol){
+        line.push(point)
+        return true;
+    }
+    return false;
+}
+
+function updateGameStatus () {
+    console.log(fieldSize ** 2 / 2);
+    console.log(moveCounter);
+    if (fieldSize ** 2 == moveCounter) {
+        alert('Победила дружба');
+        isGameOver = true;
+    } else if (isFieldExtensionEnabled && moveCounter > fieldSize ** 2 / 2) {
+        renderExtendedField();
+        updateListCell();
+    }
+}
+
+function renderVictorySymbols (symbol, winner, color) {
+    for(let i =0; i < winner.length; i++){
+        renderSymbolInCell(symbol, winner[i].x, winner[i].y, color);
+    }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -54,34 +293,8 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
+    startGame();
     console.log('reset!');
-}
-
-
-/* Test Function */
-/* Победа первого игрока */
-function testWin () {
-    clickOnCell(0, 2);
-    clickOnCell(0, 0);
-    clickOnCell(2, 0);
-    clickOnCell(1, 1);
-    clickOnCell(2, 2);
-    clickOnCell(1, 2);
-    clickOnCell(2, 1);
-}
-
-/* Ничья */
-function testDraw () {
-    clickOnCell(2, 0);
-    clickOnCell(1, 0);
-    clickOnCell(1, 1);
-    clickOnCell(0, 0);
-    clickOnCell(1, 2);
-    clickOnCell(1, 2);
-    clickOnCell(0, 2);
-    clickOnCell(0, 1);
-    clickOnCell(2, 1);
-    clickOnCell(2, 2);
 }
 
 function clickOnCell (row, col) {
