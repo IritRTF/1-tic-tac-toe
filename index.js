aff6d06884b7b22d1a804fe2d341861231493c10
Saw4uk <87923228+Saw4uk@users.noreply.github.com>
2022-09-22 22:50:26 +0500

    Final

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const ZERO = 'O';
 const EMPTY = ' ';
 const DIMENSION = prompt('Укажите размер поля:',3)
 let grid = [];
+let collumns = [];//[cross,zero]
+let rows = [];//[cross,zero]
+let mainDiagonal = [0,0];//[cross,zero]
+let secondDiagonal = [0,0];//[cross,zero]
+let isGidAutomaticlyGrow = confirm('Вы хотите, чтобы поле раширялось во время игры ?');
 let canPlay = true;
 let currentClickedCells = 0;
 
@@ -32,54 +37,6 @@ function renderGrid (dimension) {
     
 }
 
-function cellClickHandler (row, col) {
-    if(canPlay)
-    {
-        console.log(`Clicked on cell: ${row}, ${col}`);
-        if (grid[row][col] === EMPTY) {
-            currentClickedCells++;
-            renderSymbolInCell(CROSS, row, col);
-            grid[row][col] = CROSS;
-        }
-        else return;
-
-        if (winCheck() == CROSS) {
-            alert("Cross wins");
-            canPlay = false;
-            return;
-        }
-        else if (currentClickedCells == grid.length*grid.length)
-        {
-            alert("Победила дружба")
-            canPlay = false;
-            return;
-        }
-        if(canPlay)
-        {
-            makeTurn();
-            if (winCheck() == ZERO) {
-                alert("Zero wins");
-                canPlay = false;
-            }
-            else if (currentClickedCells == grid.length*grid.length)
-            {
-                alert("Победила дружба")
-                canPlay = false;
-                return;
-            }
-        }
-
-        /* Пользоваться методом для размещения символа в клетке так:
-            renderSymbolInCell(ZERO, row, col);
-        */
-        if(currentClickedCells > (grid.length * grid.length) / 2)
-        {
-            renderGrid(grid.length+1);
-        }
-    }
-}
-
-
 function generateGrid(newLength)
 {
     for (let row = 0; row < grid.length; row++) {
@@ -90,111 +47,44 @@ function generateGrid(newLength)
 
     for (let row = grid.length; row < newLength; row++) {
         grid[row] = [];
+        rows.push([0,0]);
+        collumns.push([0,0]);
         for (let col = 0; col < newLength; col++) {
             grid[row].push(EMPTY);
         }     
     } 
 }
 
-/*
-function generateGrid(dimension)
-{
-    for (let i = grid.length; i < dimension; i++) {
-        grid[i] = [];
-        for (let y = grid[i].length; y < dimension;y++)
-        {
-            if(grid[i][y] != CROSS && grid[i][y] != CROSS)
-                grid[i][y] = EMPTY;
+function cellClickHandler (row, col) {
+    if(canPlay)
+    {
+        console.log(`Clicked on cell: ${row}, ${col}`);
+        if (grid[row][col] === EMPTY) {
+            addToMeta(row,col,CROSS);
         }
+        else return;
+        if (checkWin(CROSS)) return;
+        if(currentClickedCells > (grid.length * grid.length) / 2 && isGidAutomaticlyGrow)
+        {
+            renderGrid(grid.length+1);
+        }
+        MakeIITurn();
+        if (checkWin(ZERO)) return;
     }
 }
-*/
-function isGridDontHaveEmptySlot()
+
+function checkWin(type)
 {
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[i].length; j++) {
-            if(grid[i][j] != EMPTY)
-            {
-                return false;
-            }
-        }
+    if (winCheck() == type) {
+        alert( type + " wins");
+        canPlay = false;
+        return true;
     }
-    return true;
-}
-
-function winCheck()
-{
-    for (let row = 0; row < grid.length; row++) {
-        let firstElementOfString = grid[row][0];
-        if(firstElementOfString != EMPTY) {
-            for (let col = 1; col < grid[row].length; col++) {
-                if (grid[row][col] != firstElementOfString) {
-                    break;
-                } else if (col == grid[row].length - 1) {
-                    drawRow(row)
-                    return firstElementOfString;
-                }
-            }
-        }
-
-        let firstElementOfColumn = grid[0][row]
-        if(firstElementOfColumn != EMPTY)
-        {
-            for (let realRow = 0; realRow < grid[0].length; realRow++) {
-                if(grid[realRow][row] != firstElementOfColumn)
-                {
-                    break;
-                }
-                else if(realRow == grid[0].length - 1)
-                {
-                    drawColumn(row);
-                    return firstElementOfColumn;
-                }
-            }
-        }
-
-        let mainDiagonalFirstElement = grid[0][0]
-        if(mainDiagonalFirstElement != EMPTY)
-        {
-            for (let i = 0; i < grid[0].length; i++) {
-                for (let j = 0; j < grid[0].length; j++) {
-                    if(i === j)
-                    {
-                        if(grid[i][j] != mainDiagonalFirstElement) {
-                            i = grid.length;
-                            break;
-                        }
-                        else if(i == grid.length - 1 && j == grid.length - 1)
-                        {
-                            drawMainDiadonal();
-                            return grid[grid.length - 1][grid.length - 1];
-                        }
-                    }
-                }
-            }
-        }
-
-        let secondDiagonalFirstElement = grid[0][grid.length - 1]
-        if(secondDiagonalFirstElement != EMPTY)
-        {
-            for (let i = 0; i < grid.length; i++) {
-
-                for (let j = 0; j < grid.length; j++) {
-                    if(i + j == grid.length - 1)
-                    {
-                        if(grid[i][j] != secondDiagonalFirstElement) {
-                            i = grid.length;
-                            break;
-                        }
-                        else if(i == grid.length - 1 && j == 0)
-                        {
-                            drawSecondDiadonal();
-                            return grid[grid.length - 1][0];
-                        }
-                    }
-                }
-            }
-        }
+    else if (currentClickedCells == grid.length*grid.length)
+    {
+        alert("Победила дружба")
+        canPlay = false;
+        return true;
     }
 }
 
@@ -241,6 +131,176 @@ function drawRow(j)
     }
 }
 
+function resetClickHandler () {
+    clearMap();
+    startGame();
+    console.log('reset!');
+}
+
+function winCheck() {
+    for (let indexer = 0; indexer < rows.length; indexer++) {
+        let check = checkMeta(rows[indexer]);
+        if(check[0]) 
+        {
+            drawRow(indexer);
+            return check[1];
+        }
+        else
+        {
+            check = checkMeta(collumns[indexer]);
+            if(check[0]) 
+            {
+                drawColumn(indexer);
+                return check[1];
+            }
+        }
+    }
+    let diagonalCheck = checkMeta(mainDiagonal);
+    if(diagonalCheck[0]) 
+    {
+        drawMainDiadonal();
+        return diagonalCheck[1];
+    }
+    else
+    {
+        diagonalCheck = checkMeta(secondDiagonal);
+        if(diagonalCheck[0]) 
+        {
+            drawSecondDiadonal();
+            return diagonalCheck[1];
+        }
+    }
+}
+
+function checkMeta(massive)
+{
+    if (massive[0] == grid.length)
+    {
+        return [true,CROSS];
+    }
+    else if(massive[1] == grid.length)
+    {
+        return [true,ZERO];
+    }
+    else
+    {
+        return [false,EMPTY];
+    }
+}
+
+function clickOnCell (row, col) {
+    findCell(row, col).click();
+}
+
+function clearMap()
+{
+    currentClickedCells = 0;
+    grid = [];
+    canPlay = true;
+    rows = [];
+    collumns = [];
+    mainDiagonal = [0,0];
+    secondDiagonal = [0,0];
+}
+
+function addToMeta(row,col,type)
+{
+    let numElement = (type === CROSS) ? 0 : 1;
+    currentClickedCells++;
+    renderSymbolInCell(type, row, col);
+    grid[row][col] = type;
+    rows[row][numElement] += 1;
+    collumns[col][numElement] += 1;
+    if(row === col)
+    {
+        mainDiagonal[numElement] += 1;
+    }
+
+    if(row + col === grid.length - 1)
+    {
+        secondDiagonal[numElement] += 1;
+    }
+}
+
+function MakeIITurn()
+{
+    for (let row = 0; row < rows.length; row++) {
+        if(rows[row][1] == grid.length - 1)
+        {
+            for (let col = 0; col < grid.length; col++) {
+                if(grid[row][col] === EMPTY)
+                {
+                    makeTurn(row,col)
+                    return;
+                }
+            }
+        }
+        if(collumns[row][1] == grid.length - 1)
+        {
+            for (let realRow = 0; realRow < grid.length; realRow++) {
+                if(grid[realRow][row] === EMPTY)
+                {
+                    makeTurn(realRow,row)
+                    return;
+                }
+            }
+        }
+    }
+    if(mainDiagonal[1] === grid.length - 1)
+    {
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid.length; j++) {
+                if(i === j && grid[i][j] === EMPTY)
+                {
+                    makeTurn(ZERO,i,j);
+                    return;
+                }
+            }
+        }
+    }
+    else if (secondDiagonal[1] === grid.length - 1)
+    {
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid.length; j++) {
+                if(i + j === grid.length - 1)
+                {
+                    makeTurn(ZERO,i,j);
+                    return;
+                }
+            }
+        }
+    }
+    makeTurn();
+}
+
+function makeTurn(row, column)
+{
+    let random = []
+    isPlayerTurn = true;
+    if(arguments.length === 0)
+    {
+        let randomizedDots = [];
+        for (let row = 0; row < grid.length; row++) {
+            for (let col = 0; col < grid.length; col++) {
+                if(grid[row][col] === EMPTY)
+                {
+                    randomizedDots.push([row,col])
+                }
+            }
+        }
+        random = arrayRandElement(randomizedDots);
+    }
+    else{
+        random = [row,column];
+    }
+    addToMeta(random[0],random[1],ZERO);
+}
+
+function arrayRandElement(arr) {
+    var rnd = Math.floor(Math.random() * arr.length);
+    return arr[rnd];
+}
+
 function renderSymbolInCell (symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
 
@@ -256,151 +316,4 @@ function findCell (row, col) {
 function addResetListener () {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
-}
-
-function resetClickHandler () {
-    currentClickedCells = 0;
-    grid = [];
-    startGame();
-    canPlay = true;
-    console.log('reset!');
-}
-
-
-/* Test Function */
-/* Победа первого игрока */
-function testWin () {
-    clickOnCell(0, 2);
-    clickOnCell(0, 0);
-    clickOnCell(2, 0);
-    clickOnCell(1, 1);
-    clickOnCell(2, 2);
-    clickOnCell(1, 2);
-    clickOnCell(2, 1);
-}
-
-/* Ничья */
-function testDraw () {
-    clickOnCell(2, 0);
-    clickOnCell(1, 0);
-    clickOnCell(1, 1);
-    clickOnCell(0, 0);
-    clickOnCell(1, 2);
-    clickOnCell(1, 2);
-    clickOnCell(0, 2);
-    clickOnCell(0, 1);
-    clickOnCell(2, 1);
-    clickOnCell(2, 2);
-}
-
-function clickOnCell (row, col) {
-    findCell(row, col).click();
-}
-/*
-function makeIITurn()
-{
-    for (let row = 0; row < grid.length; row++) {
-        let firstElementOfString = grid[row][0];
-        if(firstElementOfString != EMPTY) {
-            for (let col = 1; col < grid[row].length; col++) {
-                if (col == grid[row].length - 1) {
-                    makeTurn(firstElementOfString,row,col)
-                    return;
-                }
-                if (grid[row][col] != firstElementOfString) {
-                    break;
-                }
-            }
-        }
-
-        let firstElementOfColumn = grid[0][row]
-        if(firstElementOfColumn != EMPTY)
-        {
-            for (let realRow = 0; realRow < grid[0].length; realRow++) {
-                if(realRow == grid[0].length - 1)
-                {
-                    makeTurn(firstElementOfColumn,realRow,row)
-                    return;
-                }
-                if(grid[realRow][row] != firstElementOfColumn)
-                {
-                    break;
-                }
-            }
-        }
-
-        let mainDiagonalFirstElement = grid[0][0]
-        if(mainDiagonalFirstElement != EMPTY)
-        {
-            for (let i = 0; i < grid[0].length; i++) {
-                for (let j = 0; j < grid[0].length; j++) {
-                    if(i === j)
-                    {
-                        if(i == grid.length - 1 && j == grid.length - 1)
-                        {
-                            makeTurn(mainDiagonalFirstElement,i,j)
-                            return;
-                        }
-                        if(grid[i][j] != mainDiagonalFirstElement) {
-                            i = grid.length;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
-        let secondDiagonalFirstElement = grid[0][grid.length - 1]
-        if(secondDiagonalFirstElement != EMPTY)
-        {
-            for (let i = 0; i < grid.length; i++) {
-
-                for (let j = 0; j < grid.length; j++) {
-                    if(i + j == grid.length - 1)
-                    {
-                        if(i == grid.length - 1 && j == 0)
-                        {
-                            makeTurn(secondDiagonalFirstElement,i,j);
-                            return;
-                        }
-                        if(grid[i][j] != secondDiagonalFirstElement) {
-                            i = grid.length;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    makeTurn();
-}
-*/
-function makeTurn(type,row, column)
-{
-    currentClickedCells++;
-    let random = []
-    isPlayerTurn = true;
-    if(arguments.length === 0)
-    {
-        let randomizedDots = [];
-        for (let row = 0; row < grid.length; row++) {
-            for (let col = 0; col < grid.length; col++) {
-                if(grid[row][col] === EMPTY)
-                {
-                    randomizedDots.push([grid[row][col],row,col])
-                }
-            }
-        }
-        random = arrayRandElement(randomizedDots);
-    }
-    else{
-        random = [type,row,column];
-    }
-    grid[random[1]][random[2]] = ZERO;
-    renderSymbolInCell(ZERO,random[1],random[2]);
-}
-
-function arrayRandElement(arr) {
-    var rnd = Math.floor(Math.random() * arr.length);
-    return arr[rnd];
 }
