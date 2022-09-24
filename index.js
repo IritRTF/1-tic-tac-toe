a1a155e6f7118ddc46c9e4fee6aedd0c4911bd64
Roflmax <113230933+Roflmax@users.noreply.github.com>
2022-09-24 15:58:51 +0500

    Бутаков Максим 9/12

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
+let pole;
+let dimension;
+let movesLeft;
+let isGameOver;
 
 const container = document.getElementById('fieldWrapper');
 
@@ -8,17 +12,23 @@ startGame();
 addResetListener();
 
 function startGame () {
-    renderGrid(3);
+    dimension = prompt('Укадиие размер поля')
+    movesLeft = dimension**2
+    pole = [];
+    isGameOver = false;
+    renderGrid(dimension);
 }
 
-function renderGrid (dimension) {
+function renderGrid (dimension) {  
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
         const row = document.createElement('tr');
+        pole.push([]);
         for (let j = 0; j < dimension; j++) {
             const cell = document.createElement('td');
             cell.textContent = EMPTY;
+            pole[i][j]=EMPTY;
             cell.addEventListener('click', () => cellClickHandler(i, j));
             row.appendChild(cell);
         }
@@ -28,12 +38,113 @@ function renderGrid (dimension) {
 
 function cellClickHandler (row, col) {
     // Пиши код тут
+    if (isGameOver){
+        return
+    }
+    if (findCell(row, col).textContent === EMPTY){
+        if (movesLeft % 2 == 0){
+            makeMove(CROSS, row, col);
+        }
+        else{
+            makeMove(ZERO, row, col);
+        }
+    }
     console.log(`Clicked on cell: ${row}, ${col}`);
 
 
     /* Пользоваться методом для размещения символа в клетке так:
         renderSymbolInCell(ZERO, row, col);
      */
+}
+
+function makeMove(symbol, row, col){
+    renderSymbolInCell(symbol, row, col)
+    pole[row][col] = symbol;
+    movesLeft--;
+    if (checkWinner(symbol, row, col)){
+        if (symbol === CROSS){
+            alert('Победили крестики!');  
+        }
+        if (symbol === ZERO){
+            alert('Победили нолики!');
+        }
+        isGameOver = true
+    }
+    if (movesLeft === 0 && !isGameOver)
+        alert('Победила дружба!');
+}
+
+
+function checkWinner (symbol, row, col) {
+    let Found_win = true;
+    let winLine;
+    for (let i = 0; i < dimension; i++) {
+        if (pole[row][i] !== symbol) {
+            Found_win = false;
+            break;
+        }
+        Found_win = true;
+    }
+    winLine = "вертикаль";
+
+    if (!Found_win) {
+        for (let i = 0; i < dimension; i++) {
+            if (pole[i][col] !== symbol) {
+                Found_win = false;
+                break;
+            }
+            Found_win = true;
+        }
+        winLine = "горизонталь";
+    }
+
+    if (!Found_win) {
+        for (let i = 0; i < dimension; i++) {
+            if (pole[i][i] !== symbol) {
+                Found_win = false;
+                break;
+            }
+            Found_win = true;
+        }
+        winLine = "диоганаль1";
+    }
+
+    if (!Found_win) {
+        for (let i = 0; i < dimension; i++) {
+            if (pole[i][dimension - (1 + i)] !== symbol) {
+                Found_win = false;
+                break;
+            }
+            Found_win = true;
+        }
+        winLine = "диоганаль2";
+    }
+
+    if (Found_win)
+        colorWinningLine(winLine, row, col);
+    return Found_win;
+}
+
+function colorWinningLine(winLine, row, col){
+    for (let i = 0; i < dimension; i++) {
+        switch (winLine) {
+            case "вертикаль":
+                changeColor(findCell(row, i));
+                break;
+
+            case "горизонталь":
+                changeColor(findCell(i, col));
+                break;
+
+            case "диоганаль1":
+                changeColor(findCell(i, i));
+                break;
+
+            case "диоганаль2":
+                changeColor(findCell(i, dimension - (i + 1)));
+                break;
+        }
+    }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -55,8 +166,13 @@ function addResetListener () {
 
 function resetClickHandler () {
     console.log('reset!');
+    startGame();
 }
 
+function changeColor(cell){
+    cell.style.backgroundColor = '#b43030';
+    cell.style.color = 'white';
+}
 
 /* Test Function */
 /* Победа первого игрока */
