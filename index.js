b61c1473b5087e3ab383deddb706dffa60328286
ograbko <ograbko@shoppinglive.ru>
2023-05-24 02:53:40 +0600

    1-10

--- index.js
+++ index.js
@@ -2,13 +2,112 @@ const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
 
+class Field {
+    constructor(size = 3) {
+        this._size = size;
+        this._field = [];
+        this._defSymbol = EMPTY;
+        this._generateField();
+    }
+
+    get emptyCells() {
+        const empties = [];
+        for (let row = 0; row < this._size; row++) {
+            for (let cell = 0; cell < this._size; cell++) {
+                if (this._field[row][cell] === this._defSymbol) empties.push([row, cell]);
+            }
+        }
+        return empties;
+    }
+
+    get hasEmptyCells() {
+        return this.emptyCells.length > 0;
+    }
+
+    tryFillCell(row, cell, symbol) {
+        if (this._field[row][cell] !== this._defSymbol) return false;
+        this._field[row][cell] = symbol;
+        return true;
+    }
+
+    checkCombinations(row, cell) {
+        const symbol = this._field[row][cell];
+        return this._checkHorizontal(row, symbol)
+            || this._checkVertical(cell, symbol)
+            || this._checkDiagonals(row, cell);
+    }
+
+    reset() {
+        this._generateField();
+    }
+
+    _checkHorizontal(row, symbol) {
+        const combination = [];
+        for (let cell = 0; cell < this._size; cell++) {
+            if (this._field[row][cell] !== symbol) return null;
+            combination.push([row, cell]);
+        }
+        return combination;
+    }
+
+    _checkVertical(cell, symbol) {
+        const compbination = [];
+        for (let row = 0; row < this._size; row++) {
+            if (this._field[row][cell] !== symbol) return null;
+            compbination.push([row, cell]);
+        }
+        return compbination;
+    }
+
+    _checkDiagonals(row, cell) {
+        if (!this._isOnMainDiagonal(row, cell) && !this._isOnSecDiagonal(row, cell)) return null;
+        const symbol = this._field[row][cell];
+        let mainCombination = [];
+        let secCombination = [];
+        for (let i = 0; i < this._size; i++) {
+            for (let j = 0; j < this._size; j++) {
+                if (this._isOnMainDiagonal(i, j) && this._field[i][j] === symbol) mainCombination.push([i, j]);
+                if (this._isOnSecDiagonal(i, j) && this._field[i][j] === symbol) secCombination.push([i, j]);
+            }
+        }
+        return mainCombination.length === this._size 
+            ? mainCombination 
+            : secCombination.length === this._size 
+            ? secCombination
+            : null;
+    }
+
+    _isOnMainDiagonal(row, cell) {
+        return row === cell;
+    }
+    _isOnSecDiagonal(row, cell) {
+        return row + cell === this._size - 1;
+    }
+
+    _generateField() {
+        for (let row = 0; row < this._size; row++) {
+            this._field[row] = [];
+            for (let cell = 0; cell < this._size; cell++) {
+                this._field[row][cell] = this._defSymbol;
+            }
+        }
+    }
+}
+let gameIsOver = false;
+let field = null;
+const ai = ZERO;
+
 const container = document.getElementById('fieldWrapper');
 
 startGame();
 addResetListener();
 
 function startGame () {
-    renderGrid(3);
+    const size = parseInt(prompt('Размер поля (по умолчанию 3):')) || 3;
+    renderGrid(size);
+    field = new Field(size);
+    currentPlayer = CROSS;
+    gameIsOver = false;
 }
 
 function renderGrid (dimension) {
@@ -27,13 +126,39 @@ function renderGrid (dimension) {
 }
 
 function cellClickHandler (row, col) {
-    // Пиши код тут
     console.log(`Clicked on cell: ${row}, ${col}`);
+    if (gameIsOver) return;
+    const filled = field.tryFillCell(row, col, currentPlayer);
+    if (!filled) return;
+    renderSymbolInCell(currentPlayer, row, col);
+    const winnerCombo = field.checkCombinations(row, col);
+    if (winnerCombo) renderCombination(currentPlayer, winnerCombo, '#f00');
+    gameIsOver = winnerCombo || !field.hasEmptyCells;
+    if (gameIsOver) showGameOver(winnerCombo ? currentPlayer : null);
+    else togglePlayer();
+}
 
+function showGameOver(winner = null) {
+    setTimeout(() => {
+        alert(winner ? `Победил ${winner}` : 'Победила дружба');
+    })
+}
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+function togglePlayer() {
+    currentPlayer = currentPlayer === CROSS ? ZERO : CROSS;
+    if (currentPlayer === ai) aiMove();
+}
+
+function aiMove() {
+    const index = Math.floor(Math.random() * field.emptyCells.length);
+    const cell = field.emptyCells[index];
+    cellClickHandler(cell[0], cell[1])
+}
+
+function renderCombination(symbol, combination, color = '#333') {
+    for (let coords of combination) {
+        renderSymbolInCell(symbol, coords[0], coords[1], color);
+    }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -55,6 +180,7 @@ function addResetListener () {
 
 function resetClickHandler () {
     console.log('reset!');
+    startGame();
 }
 
 
